070f2a7b745c78c55c84d23cc723a59336330804
Wstudiawan <manusiasampah7@gmail.com>
2021-09-29 00:57:32 +0530

    edit anykernel.sh

--- anykernel-real.sh
+++ anykernel-real.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=Puncak Lawang Kernel by Wahyu Studiawan
 do.devicecheck=1
 do.modules=1
 do.systemless=1
